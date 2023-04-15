29f786b6c735dc11290fbc992ee39999b2b7dac6
rafka <rafkadyrov@gmail.com>
2023-04-15 22:42:46 +0700

    added saving, loading model and visualization for loss and accuracy

--- model.py
+++ model.py
@@ -1,9 +1,43 @@
-from typing import List
+from typing import List, Dict, Optional
 
 import torch
 from torch import nn
 import numpy as np
-from sklearn.metrics import log_loss
+from sklearn.metrics import log_loss, accuracy_score
+import matplotlib.pyplot as plt
+
+SMALL_SIZE = 14
+MEDIUM_SIZE = 16
+BIGGER_SIZE = 18
+
+plt.rc('font', size=SMALL_SIZE)          # controls default text sizes
+plt.rc('axes', titlesize=BIGGER_SIZE)     # fontsize of the axes title
+plt.rc('axes', labelsize=MEDIUM_SIZE)    # fontsize of the x and y labels
+plt.rc('xtick', labelsize=SMALL_SIZE)    # fontsize of the tick labels
+plt.rc('ytick', labelsize=SMALL_SIZE)    # fontsize of the tick labels
+plt.rc('legend', fontsize=SMALL_SIZE)    # legend fontsize
+plt.rc('figure', titlesize=BIGGER_SIZE)  # fontsize of the figure title
+
+
+def visualize_metric_per_epoch(metric: List[float], metric_name: str):
+    x = range(len(metric))
+    y = metric
+    plt.figure(figsize=(15, 10))
+    plt.xlabel('Номер итерации')
+    if metric_name == 'loss':
+        plt.ylabel('Значение функции потерь')
+    if metric_name == 'accuracy':
+        plt.ylabel('Значение точности')
+    plt.ylabel('Значение функции потерь')
+    plt.plot(x, y)
+
+
+def save_model(model: nn.Module, path: str):
+    torch.save(model, path)
+
+
+def load_model(path) -> nn.Module:
+    return torch.load(path)
 
 
 class Model(nn.Module):
